cebe513b84eae0e35d2d7f29dc35f1cb10adbafe
Andrew Owino <andystudy4@gmail.com>
2024-04-20 00:19:03 +0300

    Keep track of constants

---
+++ src/constants/data.js
@@ -0,0 +1,25 @@
+export const categories = [
+    'Groceries',
+    'Utilities',
+    'Rent',
+    'Transportation',
+    'Entertainment',
+    'Healthcare',
+    'Insurance',
+    'Dining Out',
+    'Clothing',
+    'Miscellaneous'
+];
+
+export const backgroundColors = [
+    '#ff5733', // Groceries
+    '#8e44ad', // Utilities
+    '#3498db', // Rent
+    '#27ae60', // Transportation
+    '#f39c12', // Entertainment
+    '#16a085', // Healthcare
+    '#2c3e50', // Insurance
+    '#c0392b', // Dining Out
+    '#2980b9', // Clothing
+    '#7f8c8d'  // Miscellaneous
+];
