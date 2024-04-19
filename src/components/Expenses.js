a3d56a38f3ffb1eff625c2ddf6e4ab2f5b41c209
Andrew Owino <andystudy4@gmail.com>
2024-04-19 14:54:02 +0300

    Render additional expense data

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 function Expenses({ expenses }) {
     return (
-        <table>
+        <table className="expenses-table">
+            <thead>
+                <th>Date</th>
+                <th>Expense</th>
+                <th>Amount</th>
+                <th>Category</th>
+            </thead>
             <tbody>
                 {expenses.map((expense) => (
                     <tr key={expense.id}>
+                        <td>{expense.date}</td>
                         <td>{expense.name}</td>
+                        <td>{expense.amount}</td>
+                        <td>{expense.category}</td>
                     </tr>
                 ))}
             </tbody>
